86c9be924801cba1febbf168b8f89877a1599dbd
craig <craig@oppkey.com>
2022-05-09 06:34:58 -0700

    added text

--- lib/world/intro_panel.dart
+++ lib/world/intro_panel.dart
@@ -1,8 +1,16 @@
 import 'package:flame/components.dart';
+import 'package:flame/palette.dart';
 import 'package:flutter/material.dart';
 import 'package:leena2/game/leena_game.dart';
 
 class IntroPanel extends PositionComponent with HasGameRef<LeenaGame> {
+  final String messageToLeena = 'Leena, this is your father. '
+      'My ship has crashed and the crystals that power my '
+      'engines are gone. I will be trapped here unless '
+      'you can gather 7 crystals and bring them to me. '
+      'The only vehicle that can move quickly in the gravity of '
+      'this planet is the hoverboard I made for you. '
+      'My oxygen is running low.  I hope I see you again.';
   @override
   Future<void>? onLoad() {
     var father = SpriteComponent(
@@ -10,6 +18,8 @@ class IntroPanel extends PositionComponent with HasGameRef<LeenaGame> {
         size: Vector2(gameRef.size[1] - 100, gameRef.size[1] - 100),
         position: Vector2(gameRef.size[0] / 2 + 50, 50));
     add(father);
+    add(IntroMessage(messageToLeena)
+      ..size = Vector2(400, gameRef.size[1] - 100));
 
     return super.onLoad();
   }
@@ -17,5 +27,15 @@ class IntroPanel extends PositionComponent with HasGameRef<LeenaGame> {
   @override
   void render(Canvas canvas) {
     canvas.drawColor(Colors.pink, BlendMode.src);
+  }
+}
+
+class IntroMessage extends TextBoxComponent {
+  IntroMessage(String text)
+      : super(text: text, boxConfig: TextBoxConfig(timePerChar: 0.05));
+  @override
+  void drawBackground(Canvas c) {
+    Rect rect = Rect.fromLTWH(0, 0, 400, height);
+    c.drawRect(rect, Paint()..color = Colors.red);
   }
 }
