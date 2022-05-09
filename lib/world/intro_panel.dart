fe55ff7c2fcf0173bf93e369e51ef963863d9007
craig <craig@oppkey.com>
2022-05-09 08:09:09 -0700

    updated intro screen

--- lib/world/intro_panel.dart
+++ lib/world/intro_panel.dart
@@ -1,13 +1,12 @@
 import 'package:flame/components.dart';
-import 'package:flame/palette.dart';
 import 'package:flutter/material.dart';
 import 'package:leena2/game/leena_game.dart';
 
 class IntroPanel extends PositionComponent with HasGameRef<LeenaGame> {
-  final String messageToLeena = 'Leena, this is your father. '
-      'My ship has crashed and the crystals that power my '
+  final String messageToLeena = 'Leena, this is your  father. '
+      'My ship has crashed and the crystals  that power my '
       'engines are gone. I will be trapped here unless '
-      'you can gather 7 crystals and bring them to me. '
+      'you can gather 7 magic crystals and bring them to me. '
       'The only vehicle that can move quickly in the gravity of '
       'this planet is the hoverboard I made for you. '
       'My oxygen is running low.  I hope I see you again.';
@@ -34,6 +33,8 @@ class IntroMessage extends TextBoxComponent {
   IntroMessage(String text, double width)
       : super(
             text: text,
+            textRenderer: TextPaint(
+                style: const TextStyle(fontSize: 32.0, fontFamily: 'Arcade')),
             boxConfig: TextBoxConfig(maxWidth: width, timePerChar: 0.05));
   @override
   void drawBackground(Canvas c) {
